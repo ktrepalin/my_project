4bdd119545ba113372d25717293a9346ffec2f70
Unknown <k.trepalin@yandex.ru>
2018-03-18 18:00:34 +0300

    Merge branch 'feature/static-pages'
    navbar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,12 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
