973370d9a0bdecb4d599467008c447eedffc045b
Unknown <k.trepalin@yandex.ru>
2018-04-22 16:08:11 +0300

    2204

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,18 @@
 class StaticPagesController < ApplicationController
+  def service
+  end
+  
   def home
   end
-
+ 
   def help
   end
-	def full_title(page_title)
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
-	end
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
+
+  root 'static_pages#home'
+
   get "static_pages/home"
   get "static_pages/help"
+  match '/service', to: 'static_pages#service', via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
